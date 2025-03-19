f5e7e07e027cf94c1dca160d8f3f95752b7378a4
DhananjayaBandara <145431367+DhananjayaBandara@users.noreply.github.com>
2025-03-19 10:49:35 +0530

    Creating serializers

--- blogapitutorial/blogApi/blogApiApp/views.py
+++ blogapitutorial/blogApi/blogApiApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -11,4 +12,6 @@ def index(request):
 @api_view(['GET'])
 def getAllPosts(request):
     get_posts = Post.objects.all()
-    return Response(get_posts)
+    serializer = PostSerializer(get_posts)
+    
+    return Response(serializer.data)
