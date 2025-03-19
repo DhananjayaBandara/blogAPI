cc03f033549dd006f0c952d94c75d679e453da5e
DhananjayaBandara <145431367+DhananjayaBandara@users.noreply.github.com>
2025-03-19 09:22:01 +0530

    Register the blogApiApp in settings.py

--- blogapitutorial/blogApi/blogApi/settings.py
+++ blogapitutorial/blogApi/blogApi/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'blogApiApp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
